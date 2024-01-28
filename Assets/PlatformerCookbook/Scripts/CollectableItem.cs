ac035dccaa2e5d91248a2255ea0850e91912e244
sigmaphon <sigma@tailerderden.com>
2024-01-28 11:55:14 +0300

    dfhfghi

---
+++ Assets/PlatformerCookbook/Scripts/CollectableItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CollectableItem : MonoBehaviour
+{
+    public UnityEvent action;
+    public bool destroyAfterCollected = true;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var controller = other.GetComponent<PlatformerCharacterController>();
+        if (!controller) return;
+
+        action.Invoke();
+        
+        if (!destroyAfterCollected) return;
+        Destroy(gameObject);
+
+    }
+    public void Cringe(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, transform.position);
+    }
+}
